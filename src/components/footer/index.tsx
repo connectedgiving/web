a729b70ac78714a77696e08de336c1163f46a962
Ram Gopinathan <seafireball@Rams-MacBook-Pro.local>
2021-12-09 20:59:47 -0600

    Initial create

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Footer extends React.Component{
+
+    public render() {
+
+        return(
+            <p>Footer</p>
+        )
+    }
+}
---
+++ src/components/header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Header extends React.Component {
+    public render() {
+        return(
+            <p>Header</p>
+        )
+    }
+}
+
+export default Header;
